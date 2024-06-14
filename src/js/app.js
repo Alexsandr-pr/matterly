d4a01e6fd1f1de809e00b39e534e024e20555c80
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-06-14 13:31:34 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+import * as flsFunctions from "./modules/functions.js";
+flsFunctions.isWebp();
+
+//import burgerMenu from "./ui/burger.js";
+//burgerMenu();
+
+
+import  accordion  from "./ui/accordion.js";
+accordion();
+
+//import tabs from "./ui/tabs.js";
+//tabs(triggerClass, contentClass);
+
+//import DynamicAdapt from "./modules/dynamicadapt.js";
+//const da = new DynamicAdapt("max");
+//da.init();
+
+
+
+
+
+
+
+
+
+
+
+
